7652f6286d7be1fbd1c50bb37b57966de004d6f1
Jimmy Bogard <jimmy.bogard@gmail.com>
2020-11-30 08:05:01 -0600

    Updating package

--- src/MongoDB.Driver.Core.Extensions.OpenTelemetry/TracerProviderBuilderExtensions.cs
+++ src/MongoDB.Driver.Core.Extensions.OpenTelemetry/TracerProviderBuilderExtensions.cs
@@ -1,22 +1,10 @@
 ﻿using System;
-using OpenTelemetry.Trace;
 
-namespace MongoDB.Driver.Core.Extensions.OpenTelemetry
+namespace OpenTelemetry.Trace
 {
     public static class TracerProviderBuilderExtensions
     {
         public static TracerProviderBuilder AddMongoDBInstrumentation(this TracerProviderBuilder builder)
-            => builder.AddMongoDBInstrumentation(null);
-
-        public static TracerProviderBuilder AddMongoDBInstrumentation(this TracerProviderBuilder builder, Action<MongoDBInstrumentationOptions> configureInstrumentationOptions)
-        {
-            configureInstrumentationOptions ??= opt => { };
-
-            var options = new MongoDBInstrumentationOptions();
-
-            configureInstrumentationOptions(options);
-
-            return builder.AddInstrumentation(t => new MongoDBCommandAdapter(t, options));
-        }
+            => builder.AddSource("MongoDB.Driver.Core.Extensions.DiagnosticSources");
     }
 }
